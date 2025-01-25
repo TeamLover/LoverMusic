f09e1394dd020bfaa64c46efd0e1322868e2517b
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-25 08:51:05 +0530

    Update help.py

--- LoverMusic/plugins/bot/help.py
+++ LoverMusic/plugins/bot/help.py
@@ -3,11 +3,11 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from AviaxMusic import app
-from AviaxMusic.utils import help_pannel
-from AviaxMusic.utils.database import get_lang
-from AviaxMusic.utils.decorators.language import LanguageStart, languageCB
-from AviaxMusic.utils.inline.help import help_back_markup, private_help_panel
+from LoverMusic import app
+from LoverMusic.utils import help_pannel
+from LoverMusic.utils.database import get_lang
+from LoverMusic.utils.decorators.language import LanguageStart, languageCB
+from LoverMusic.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_GROUP
 from strings import get_string, helpers
 
