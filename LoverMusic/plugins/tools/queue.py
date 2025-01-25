ab2964f9344863fd39aa58d608b959e560571a4b
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-25 09:29:40 +0530

    Update queue.py

--- LoverMusic/plugins/tools/queue.py
+++ LoverMusic/plugins/tools/queue.py
@@ -6,12 +6,12 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 import config
-from AviaxMusic import app
-from AviaxMusic.misc import db
-from AviaxMusic.utils import AviaxBin, get_channeplayCB, seconds_to_min
-from AviaxMusic.utils.database import get_cmode, is_active_chat, is_music_playing
-from AviaxMusic.utils.decorators.language import language, languageCB
-from AviaxMusic.utils.inline import queue_back_markup, queue_markup
+from LoverMusic import app
+from LoverMusic.misc import db
+from LoverMusic.utils import LoverBin, get_channeplayCB, seconds_to_min
+from LoverMusic.utils.database import get_cmode, is_active_chat, is_music_playing
+from LoverMusic.utils.decorators.language import language, languageCB
+from LoverMusic.utils.inline import queue_back_markup, queue_markup
 from config import BANNED_USERS
 
 basic = {}
