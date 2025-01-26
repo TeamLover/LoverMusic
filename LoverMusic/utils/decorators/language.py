23d4040288812fa032660b5162d24eb293407991
TeamLover <123320837+TeamLover@users.noreply.github.com>
2025-01-26 10:33:35 +0530

    Update language.py

--- LoverMusic/utils/decorators/language.py
+++ LoverMusic/utils/decorators/language.py
@@ -1,5 +1,5 @@
-from AviaxMusic.misc import SUDOERS
-from AviaxMusic.utils.database import get_lang, is_maintenance
+from LoverMusic.misc import SUDOERS
+from LoverMusic.utils.database import get_lang, is_maintenance
 from config import SUPPORT_GROUP
 from strings import get_string
 
