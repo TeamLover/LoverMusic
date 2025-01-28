41bcfe436aebd67bb0fdc017bbb62345723936de
TeamLover <123320837+TeamLover@users.noreply.github.com>
2025-01-28 15:15:48 +0530

    Update pause.py

--- LoverMusic/plugins/admins/pause.py
+++ LoverMusic/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AviaxMusic import app
-from AviaxMusic.core.call import Aviax
-from AviaxMusic.utils.database import is_music_playing, music_off
-from AviaxMusic.utils.decorators import AdminRightsCheck
-from AviaxMusic.utils.inline import close_markup
+from LoverMusic import app
+from LoverMusic.core.call import Lover
+from LoverMusic.utils.database import is_music_playing, music_off
+from LoverMusic.utils.decorators import AdminRightsCheck
+from LoverMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Aviax.pause_stream(chat_id)
+    await Lover.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
