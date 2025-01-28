e067cae8ca4ef139e60f709a9c6d76fd1ea8231b
TeamLover <123320837+TeamLover@users.noreply.github.com>
2025-01-28 09:36:49 +0530

    Update Youtube.py

--- LoverMusic/platforms/Youtube.py
+++ LoverMusic/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AviaxMusic.utils.database import is_on_off
-from AviaxMusic.utils.formatters import time_to_seconds
+from LoverMusic.utils.database import is_on_off
+from LoverMusic.utils.formatters import time_to_seconds
 
 
 
