7fd610cbdda109f891934153785a636d90302063
TeamLover <123320837+TeamLover@users.noreply.github.com>
2025-02-06 12:34:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Lover-Music/Newrepo",
+    "https://github.com/TeamLover/LoverMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
