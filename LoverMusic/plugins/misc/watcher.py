c68bc813ef605790501a1f754c7d93dfa2be1af6
TeamLover <123320837+TeamLover@users.noreply.github.com>
2025-02-06 11:21:39 +0530

    Update watcher.py

--- LoverMusic/plugins/misc/watcher.py
+++ LoverMusic/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AviaxMusic import app
-from AviaxMusic.core.call import Aviax
+from LoverMusic import app
+from LoverMusic.core.call import Lover
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await Aviax.stop_stream_force(message.chat.id)
+    await Lover.stop_stream_force(message.chat.id)
